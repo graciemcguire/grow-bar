a2c3b0abbc8aad3c975343b8b7171f5e9f5ad984
Joseph Lorenzo <josephclorenzo@gmail.com>
2019-07-03 23:14:56 -0400

    conditionally rendering classes from within getClassName

--- grow-bar-frontend/src/components/HomeScreen.js
+++ grow-bar-frontend/src/components/HomeScreen.js
@@ -2,12 +2,30 @@ import React, { Component } from 'react';
 
 
 class HomeScreen extends Component {
+
+    state = {
+        active: false
+    }
+
+    addActiveClass = () => {
+        this.setState({
+            active: true
+        })
+    }
+
+    getClassName = () => {
+        if(this.state.active) return "active";
+        else return "";
+    }
+
     render() {
+        console.log(this.state.active);
         return (
             <div>
                 <div className="home-container">
 
-                    <button onClick={this.props.renderContainer}> Stuff </button>
+                    <button onClick={this.addActiveClass} className={this.getClassName()}> Stuff </button>
+
                 </div>
             </div>
         );
